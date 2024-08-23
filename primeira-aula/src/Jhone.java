1edd41900635d39f262f3c597a5a0cac06a916e5
Jhonesm13 <jhone.menezes@edu.pucrs.br>
2024-08-23 00:57:30 +0000

    add fork

--- primeira-aula/src/Jhone.java
+++ primeira-aula/src/Jhone.java
@@ -1,5 +1,6 @@
 public class Jhone {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World - Jhone");
+        System.out.println("Fork");
     }
 }
